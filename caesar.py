8176d0cfa5d6c31be482c17b02d93f619c24b6c5
Clarklee2009 <38969091+Clarklee2009@users.noreply.github.com>
2021-05-17 00:03:55 -0400

    2

--- caesar.py
+++ caesar.py
@@ -1,12 +1,13 @@
 
-def encrypt(key,plaintext):
-    ciphertext=""
-    #YOUR CODE HERE
+def encrypt(key, plaintext):
+    ciphertext = ""
+    # YOUR CODE HERE
+    for i in plaintext:
+        ciphertext += chr((ord(i) + key-65) % 26 + 65)
     return ciphertext
 
-def decrypt(key,ciphertext):
-    plaintext=""
-    #YOUR CODE HERE
+
+def decrypt(key, ciphertext):
+    plaintext = ""
+    # YOUR CODE HERE
     return plaintext
-
-
