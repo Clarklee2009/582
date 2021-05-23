5cc9cbf3879be0b47b180a34e5cb3ef52e6abedd
Clarklee2009 <38969091+Clarklee2009@users.noreply.github.com>
2021-05-23 18:54:21 -0400

    week2

--- salted_hashing.py
+++ salted_hashing.py
@@ -6,9 +6,13 @@ def myhash(m):
     # Generate random nonce
     nonce = hashlib.sha256(str(m).encode('utf-8'))
     # Generate hex digest
-    h_hex = nonce.hexdigest()
+    r = nonce.hexdigest()
+    s = r + m
+    h_hex = hashlib.sha256(str(s).encode('utf-8')).hexdigest()
     return nonce, h_hex
 
 
-# data = "Chopper"
-# print(myhash(data))
+data = "Chopper"
+print(myhash(data))
+a, b = myhash(data)
+print(len(b))
