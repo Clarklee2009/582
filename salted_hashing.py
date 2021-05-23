72bea058566d722acbd360847c5e190e38db3f9d
Clarklee2009 <38969091+Clarklee2009@users.noreply.github.com>
2021-05-23 18:19:46 -0400

    Update salted_hashing.py

--- salted_hashing.py
+++ salted_hashing.py
@@ -4,11 +4,11 @@ import os
 
 def myhash(m):
     # Generate random nonce
-    nonce = hashlib.sha256()
+    nonce = hashlib.sha256(str(m).encode('utf-8'))
     # Generate hex digest
     h_hex = nonce.hexdigest()
     return nonce, h_hex
 
 
-data = "Chopper"
-print(myhash(data))
+# data = "Chopper"
+# print(myhash(data))
