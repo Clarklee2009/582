dfa59ed121022a07ab9a8bddd2bbf07d804d3081
Clarklee2009 <clarklee2009@hotmail.com>
2021-06-23 21:10:44 -0400

    Update num_BTC.py

--- num_BTC.py
+++ num_BTC.py
@@ -4,10 +4,14 @@ def num_BTC(b):
     c = float(0)
     integer = b // 210000
     v = 50
-    for i in range(integer):
-        v = 50/(2**i)
-        c += 210000 * v
-    c += (b - integer * 210000) * v /2
+    if b <= 210000:
+        c += v * b
+    else:
+        for i in range(integer):
+            v = 50/(2**i)
+            c += 210000 * v
+        c += (b - integer * 210000) * v /2
 
     
     return c
+print(num_BTC(13156))
