7910ed808620ba562befa37e82a000554b8ccca7
Clarklee2009 <clarklee2009@hotmail.com>
2021-07-12 22:57:17 -0400

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,13 +11,27 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
-    print("this is test*************************************")
-    # print(content[payload])
+#     print("this is test*************************************")
+    sig = content["sig"]
+    pk = content["payload"]["pk"]
+    payload = json.dumps(content["payload"])
 
-
+    platform = content["payload"]["platform"]
+    result = False
+#     print("this is result^^^^^^^^^^^^^", result)
+    if platform == "Ethereum":
+      eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+      if eth_account.Account.recover_message(eth_encoded_msg,signature=sig) == pk:
+          print( "Eth sig verifies!" )
+          result = True
+    elif platform == "Algorand":
+      if algosdk.util.verify_bytes(payload.encode('utf-8'),sig,pk):
+          print( "Algo sig verifies!" )
+          result = True
     #Check if signature is valid
-    result = True #Should only be true if signature validates
+#     print("this is result", result)
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(port='5002')
+    app.run(port='5002', debug=True)
+    
