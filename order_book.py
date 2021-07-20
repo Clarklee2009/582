77ced939d8899cbb27f76742b7bfbfb3e47f3605
Clarklee2009 <clarklee2009@hotmail.com>
2021-07-19 21:23:39 -0400

    Update order_book.py

--- order_book.py
+++ order_book.py
@@ -18,27 +18,27 @@ def process_order(order):
     orders = session.query(Order).filter(Order.filled != "").all() 
     for e_order in orders:
       #2 Check if there are any existing orders that match
-        if e_order.buy_currency == order['sell_currency'] and e_order.sell_currency == order['buy_currency']:
-          if e_order.sell_amount/ e_order.buy_amount >= order.buy_amount/order.sell_amount:
+        if e_order.buy_currency == order_obj.sell_currency and e_order.sell_currency == order_obj.buy_currency:
+          if e_order.sell_amount/ e_order.buy_amount >= order_obj.buy_amount/order_obj.sell_amount:
             #3.1 Set the filled field to be the current timestamp on both orders
             time = datetime.now()
-            order["filled"] = time
+            order_obj.filled = time
             e_order.filled = time
-            print(order["filled"], e_order.filled)
+            print(order_obj, e_order.filled)
             
             #3.2 Set counterparty_id to be the id of the other order
-            e_order.counterparty_id = order['id']
+            e_order.counterparty_id = order_obj.id
             session.commit()
             #3.3 if not completely filled
             
             
-            sell = order["sell_amount"]
-            if order["sell_amount"] < e_order.buy_amount:
-              c_by = e_order.id
-              n_sell = e_order.sell_amount - order["sell_amount"]
+            
+            if order_obj.sell_amount < e_order.buy_amount:
+              c_by = order_obj.id
+              n_sell = e_order.sell_amount - order_obj.sell_amount
               n_buy = n_sell * (e_order.sell_amount/e_order.buy_amount)
               new_order = Order(sender_pk=order['sender_pk'],receiver_pk=order['receiver_pk'], 
                       buy_currency=order['buy_currency'], sell_currency=order['sell_currency'], 
                       buy_amount=n_buy, sell_amount=n_sell, created_id=c_by )
-              e_order.child = new_order
+              
               break
