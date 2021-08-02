3d0e7c33cfd4e0275fb700089f4938b46fefc38b
Clarklee2009 <clarklee2009@hotmail.com>
2021-08-02 03:05:12 -0400

    Update trace_txes.py

--- trace_txes.py
+++ trace_txes.py
@@ -68,7 +68,7 @@ class TXO:
                 print("@@@@@",v)
                 print(l, i)
                 tm = rpc_connection.getrawtransaction(v['txid'],True)
-                txx = self.from_tx_hash(v['txid'])
+                txx = self.from_tx_hash(v['txid'],i)
                 # print("$$$$$$",self.tx_hash)
                 txx.inputs = tm['vin']
                 # print("&&&&&&&&&&&&&", txx.inputs)
