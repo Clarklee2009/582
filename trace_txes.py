e479be072be0d66075bda5ba4fa5ae2f35430dcc
Clarklee2009 <clarklee2009@hotmail.com>
2021-08-01 21:35:01 -0400

    Update trace_txes.py

--- trace_txes.py
+++ trace_txes.py
@@ -41,8 +41,8 @@ class TXO:
         t_list = tx['vout']
         n_list = t_list[n]
         time =  datetime.fromtimestamp(tx['time'])
-        owner = n_list["scriptPubKey"]["addresses"]
-        amount = n_list["value"] * 10000000
+        owner = n_list["scriptPubKey"]["addresses"][0]
+        amount = int(n_list["value"] * 10000000)
         n_number = n_list["n"]
         txo = TXO(tx_hash, n_number, amount, owner, time)
         return txo
